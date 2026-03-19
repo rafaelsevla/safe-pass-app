3b47ecbcaa2d64f781170da6238ed6c76f0ed78c
Rafitos <rafaelsevla@gmail.com>
2026-03-19 20:38:40 -0300

    generate entry model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_021800) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_19_233437) do
+  create_table "entries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "password"
+    t.datetime "updated_at", null: false
+    t.string "url"
+    t.integer "user_id", null: false
+    t.string "username"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +33,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_021800) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "entries", "users"
 end
