f29de7a9d5a0a1048dd1473846c616eece0bb8a1
Rafitos <rafaelsevla@gmail.com>
2026-03-19 21:24:50 -0300

    add validations to new entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,15 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validate :url_must_be_valid
+
+  private
+
+  # manual validation
+  def url_must_be_valid
+    unless url.include?("http" || "https")
+      errors.add(:url, "must be valid")
+    end
+  end
 end
