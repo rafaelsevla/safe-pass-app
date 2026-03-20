d376bfcee971528293029a3691f5432fda694821
Rafitos <rafaelsevla@gmail.com>
2026-03-19 21:44:14 -0300

    user entry#index as home with entry list

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,10 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @entries = current_user.entries
+  end
+
   def new
     @entry = Entry.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :entries
 
-  root "pages#home"
+  root "entries#index"
 end
